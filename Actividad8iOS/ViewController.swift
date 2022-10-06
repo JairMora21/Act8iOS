8851be6751dfa9603a0a14eed41acebe53e01f56
Jair <jairntonio21@hotmail.com>
2022-10-05 20:44:32 -0500

    Initial Commit

---
+++ Actividad8iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Actividad8iOS
+//
+//  Created by Alumno on 05/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
